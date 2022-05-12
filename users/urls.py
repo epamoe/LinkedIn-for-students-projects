0cf40c0cc51d26cb970c37d3aff78157058dbeb6
Mcdavens4 <81717614+Mcdavens4@users.noreply.github.com>
2022-05-12 06:04:50 +0100

    Mise à jour de la page profil_student

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register/', register, name="register"),
+    path('register_student', register_student, name="register_student"),
+    path('register_investor', register_investor, name="register_investor"),
+    path('update_profile_student', update_profile_student, name="update_profile_student"),
+    path('update_profile_invest', update_profile_invest, name="update_profile_invest"),
+    path('profil_student', profil_student, name="profil_student"),
+    path('profil_invest', profil_invest, name="profil_invest"),
+
+
 ]
